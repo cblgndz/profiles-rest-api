0d304ca2d0d633726d4b72308734ed0385e5e59d
cblgndz <cbzpetroleum@gmail.com>
2020-07-29 15:11:22 +0530

    Added deployment configs and scripts with update second

--- sars/profiles_project/profiles_project/settings.py
+++ sars/profiles_project/profiles_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'rooi81h0h!p7!*!p(femai@vuwqrm#3s3)e0el9p-x2vyqnxzt'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ec2-18-222-43-164.us-east-2.compute.amazonaws.com']
 
 
 # Application definition
